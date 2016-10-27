2cec0d8afab97f22a13eb6fe2f38f77cd5e1c91f
Grisha Bordyugov <gregbg@gmail.com>
2016-10-27 21:20:21 +0200

    working further with scala

--- sudoku.scala
+++ sudoku.scala
@@ -1,16 +1,19 @@
-val rows = 'A' to 'I' toList
-val cols = '1' to '9' toList
+object SudokuSolver {
+  val rows = 'A' to 'I' toList
+  val cols = '1' to '9' toList
 
-val digits = '1' to '9' toList
+  val digits = '1' to '9' toList
 
-def cross[A, B](as: List[A], bs: List[B]): List[(A, B)] = 
-  for(a <- as; b <- bs) yield((a, b))
+  def cross[A, B](as: List[A], bs: List[B]): List[(A, B)] = 
+    for(a <- as; b <- bs) yield((a, b))
 
-val squares = cross(rows, cols)
+  val squares = cross(rows, cols)
 
-val tmp = List(1, 2, 3) ++
-          List(3, 2, 1)
-/*
-val unitlist = (for(c <- cols) yield(cross(rows,    List(c)))) ++
-               (for(r <- rows) yield(cross(List(r), cols   )))
-               */
+  val unitlist = (for(c <- cols) yield(cross(rows,    List(c)))) ++
+                 (for(r <- rows) yield(cross(List(r), cols   ))) ++
+                 (for(r <- List("ABC", "DEF", "GHI");
+                      c <- List("123", "456", "789"))
+                  yield(cross(r.toList, c.toList)))
+  val units = Map { for (s <- squares; u <- unitlist if u contains s)
+                    yield((????))
+}
